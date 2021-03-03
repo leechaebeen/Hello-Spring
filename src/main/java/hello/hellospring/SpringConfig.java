0144c8588eb7606e0a98869fd1ea9385a30d54cd
tech7billie <tech.billie@gmail.com>
2021-03-03 13:57:15 +0900

    스프링 DB접근 기술 - JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,9 +1,6 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -15,13 +12,12 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig
 {
-
-    private DataSource dataSource;
+    private EntityManager em;
 
     @Autowired
-    public SpringConfig(DataSource dataSource)
+    public SpringConfig(EntityManager em)
     {
-        this.dataSource = dataSource;
+        this.em = em;
     }
 
     @Bean
@@ -35,7 +31,8 @@ public class SpringConfig
     {
         // return new MemoryMemberRepository();
         // return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(dataSource);
     }
 
 
